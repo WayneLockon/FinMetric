286903d26da592b3af0f3a66d6e3ad7fe5b7d8dd
Weiheng Zhang <wzhang626@wisc.edu>
2022-04-13 19:39:03 -0500

    Rewrite fred function such that it is based on getSymbols from quantmod package.

--- R/dataset.R
+++ R/dataset.R
@@ -9,10 +9,14 @@
 #'
 #' @examples
 #' fred("FEDFUNDS", "1954-01-01", "2021-08-01")
-fred <- function(Dataset = character(), start.date, end.date){
-    dat <- suppressWarnings(tidyquant::tq_get(Dataset, get = "economic.data", from = as.Date(start.date), to  = as.Date(end.date))[, -1])
-    names(dat)[ncol(dat)] <- Dataset
-    dat
+fred <- function(Dataset = character(),
+                 start.date,
+                 end.date){
+    quantmod::getSymbols(Dataset, src = "FRED", auto.assign = F, return.class = "data.frame") %>%
+        dplyr::mutate(date = as.Date(rownames(.))) %>%
+        dplyr::relocate(date) %>%
+        dplyr::filter(date >= lubridate::as_date(start.date) & date <= lubridate::as_date(end.date)) %>%
+        tibble()
 }
 
 #' Read Stata dta file from given path
